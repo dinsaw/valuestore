bdf3c4ce31b694656fec2046ce0d40ca52596b3e
dinsaw <dineshsawant300@gmail.com>
2019-01-29 16:27:50 +0530

    Use config retriever

--- launcher/src/main/java/com/github/dinsaw/valuestore/App.java
+++ launcher/src/main/java/com/github/dinsaw/valuestore/App.java
@@ -2,6 +2,7 @@ package com.github.dinsaw.valuestore;
 
 import com.github.dinsaw.valuestore.verticle.IngestVerticle;
 import com.github.dinsaw.valuestore.verticle.Server;
+import io.vertx.config.ConfigRetriever;
 import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Vertx;
 import io.vertx.core.json.JsonObject;
@@ -13,29 +14,25 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class App {
     public static void main(String[] args) {
-        Vertx vertx = Vertx.vertx();
+        Vertx serverVertx = Vertx.vertx();
 
-        DeploymentOptions options = new DeploymentOptions()
-                .setConfig(getConfig(vertx));
-        log.info("Config = {}", options.getConfig());
+        ConfigRetriever configRetriever = ConfigRetriever.create(serverVertx);
+        configRetriever.getConfig(ar -> {
+            if (ar.succeeded()) {
+                DeploymentOptions options = new DeploymentOptions()
+                        .setConfig(ar.result());
 
-        Server server = new Server();
-        vertx.deployVerticle(server, options, res -> {
-            if (res.failed()) res.cause().printStackTrace();
+                log.info("Config = {}", options.getConfig());
+                Server server = new Server();
+                serverVertx.deployVerticle(server, options, res -> {
+                    if (res.failed()) res.cause().printStackTrace();
+                });
+
+                Vertx ingestVertx = Vertx.vertx();
+                ingestVertx.deployVerticle(new IngestVerticle(), options);
+            } else {
+                log.error("Failed to get config");
+            }
         });
-
-        Vertx ingestVertx = Vertx.vertx();
-        ingestVertx.deployVerticle(new IngestVerticle(), options);
-    }
-
-    private static JsonObject getConfig(Vertx vertx) {
-        JsonObject json = vertx.getOrCreateContext().config();
-        log.info("Current Dir {}", System.getProperty("user.dir"));
-        if (json == null || json.isEmpty()) {
-            return new JsonObject(
-                    vertx.fileSystem()
-                            .readFileBlocking("conf/config.json"));
-        }
-        return json;
     }
 }
